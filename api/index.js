9e5e7aee5d9ec6a32334e4e74586320a1814063f
Hugovalpo <lillo.hugo@gmail.com>
2023-07-14 02:01:43 +0200

    vercel 4

--- api/index.js
+++ api/index.js
@@ -1,31 +1,4 @@
-require("dotenv").config();
-var express = require("express");
-//const helmet = require("helmet");
-const connection = require("./models/connection");
+import app from "../app";
 
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 
-//router
-const indexRouter = require("../routes/index");
-const articlesRouter = require("../routes/articles");
-//const brandsRouter = require("./routes/brands");
-
-var app = express();
-
-const cors = require("cors");
-app.use(cors());
-//app.use(helmet());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-// router prefixes
-app.use("/api", indexRouter);
-app.use("/api/articles", articlesRouter);
-//app.use("/brands", brandsRouter);
-
-module.exports = app;
+export default app;
